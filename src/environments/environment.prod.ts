f0c847a49d3490aad988401f95f1ebfeee266729
Mahmoudawd4 <mahmoudawd54@gmail.com>
2022-03-10 17:45:29 +0200

    api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://short-online-courses.herokuapp.com/' ,
+  baseUrl: 'https://short-online-courses.herokuapp.com/api/' ,
   publishableKeyStripe:'pk_test_51KY7OyGurDTsIveDgdUzsTOYnK2M2yuGCIsqpaSjoQVRYaIgGoSSghxfGmJHZxTH30EkMikkhZA18REaBuCHEuQE00kkYon143'
 };
