9e1e31b3a893ffb35c5d6585f60cf6b793e3a430
Mahmoudawd4 <mahmoudawd54@gmail.com>
2022-03-12 22:10:00 +0200

    edit lg k

--- src/app/navbar-main/navbar-main.component.ts
+++ src/app/navbar-main/navbar-main.component.ts
@@ -24,8 +24,8 @@ export class NavbarMainComponent implements OnInit {
 
   ngOnInit(): void {
     this.checktoken();
-    if (localStorage.getItem('role') == 'isTrainer') this.checkUser = 'trainer';
-    else if (localStorage.getItem('role') == 'isStudent') this.checkUser = 'student';
+    // if (localStorage.getItem('role') == 'isTrainer') this.checkUser = 'trainer';
+    // else if (localStorage.getItem('role') == 'isStudent') this.checkUser = 'student';
 // console.log(this.checkUser);
 
     this.studentService.studentloginservice.subscribe(
